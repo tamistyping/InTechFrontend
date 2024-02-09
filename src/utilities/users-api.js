771fe93c5287b13477562739d4a7a7a56c752e1d
tamistyping <enquiretam@gmail.com>
2024-02-09 14:22:42 +0000

    netlify update

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,15 +1,15 @@
 import sendRequest from './send-request';
-const BASE_URL = '/users';
+
 
 export function signUp(userData) {
-  return sendRequest(`${process.env.REACT_APP_BACKEND_URL}${BASE_URL}`, 'POST', userData);
+  return sendRequest(`${process.env.REACT_APP_USERS_URL}`, 'POST', userData);
 }
 
 export function login(credentials) {
-  return sendRequest(`${process.env.REACT_APP_BACKEND_URL}${BASE_URL}/login`, 'POST', credentials);
+  return sendRequest(`${process.env.REACT_APP_USERS_URL}/login`, 'POST', credentials);
 }
 
 export function checkToken(){
-  return sendRequest(`${process.env.REACT_APP_BACKEND_URL}${BASE_URL}/check-token`);
+  return sendRequest(`${process.env.REACT_APP_USERS_URL}/check-token`);
 }
 
